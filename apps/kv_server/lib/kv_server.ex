b103efb269505eb12b83c4b876dbc7971ba6c968
Le Tan Thang <letanthang@gmail.com>
2022-10-03 10:56:51 +0700

    init_proj

---
+++ apps/kv_server/lib/kv_server.ex
@@ -0,0 +1,58 @@
+defmodule KVServer do
+  require Logger
+
+  @moduledoc """
+  Documentation for `KVServer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KVServer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def accept(port) do
+    # The options below mean:
+    #
+    # 1. `:binary` - receives data as binaries (instead of lists)
+    # 2. `packet: :line` - receives data line by line
+    # 3. `active: false` - blocks on `:gen_tcp.recv/2` until data is available
+    # 4. `reuseaddr: true` - allows us to reuse the address if the listener crashes
+    {:ok, socket} =
+      :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
+
+    Logger.info("Accept connections on port #{port}")
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    {:ok, pid} = Task.Supervisor.start_child(KVServer.TaskSupervisor, fn -> serve(client) end)
+    :ok = :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data <> "===> reply by KVServer\n"
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
+  end
+end
