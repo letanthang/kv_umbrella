8a44b21feaa5aeb7ace4c531aebbae5fc6999a1c
Le Tan Thang <letanthang@gmail.com>
2022-10-04 13:45:25 +0700

    release

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,3 +1,10 @@
 import Config
 config :iex, default_prompt: ">>>"
 config :kv, :routing_table, [{?a..?z, node()}]
+
+if config_env() == :prod do
+  config :kv, :routing_table, [
+    {?a..?m, :"foo@computer-name"},
+    {?n..?z, :"bar@computer-name"}
+  ]
+end
